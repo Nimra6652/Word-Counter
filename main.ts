386719fb3bc82452ab40127a5858c799ee9cef33
Nimra <161188938+Nimra6652@users.noreply.github.com>
2024-04-25 00:02:09 +0500

    Count Words with Our Word counter

---
+++ main.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.bold.blue("\n \t Welcome to 'CODEWITHNIMRA' - Words Counter \n"));
+
+let answers = await inquirer.prompt([
+  {
+    name: "sentence",
+    type: "input",
+    message:(chalk.bgCyan("\nEnter a Sentence:")),
+  }
+]);
+
+let words = answers.sentence.trim().split(" ");
+console.log(chalk.bold.bgCyan("\nSentence Words:"));
+console.log(words);
+console.log(chalk.bold.bgCyan(`\n Word Count: ${chalk.bold.green(words.length)}`));
+
+
+ 
+
